f731313a2f894250b84536eeea9428a51c726622
mmhuman <mmhuman@mail.ru>
2017-04-19 01:29:14 +0300

    UPD

--- Creating/DDL.sql
+++ Creating/DDL.sql
@@ -1,21 +1,3 @@
-CREATE TABLE teams (
-    id serial PRIMARY KEY,
-    name text UNIQUE NOT NULL,
-    rating real NOT NULL DEFAULT 0,
-    contest_cnt integer NOT NULL DEFAULT 0,
-    member_1 serial,
-    member_2 serial,
-    member_3 serial,
-    country text,
-    organization text
-);
-
-CREATE TABLE teams_aliases (
-    id serial PRIMARY KEY,
-    name text UNIQUE NOT NULL,
-    team_id serial
-);
-
 CREATE TABLE contestants (
     id serial PRIMARY KEY,
     name text,
@@ -23,6 +5,24 @@ CREATE TABLE contestants (
     tc_profile text,
     country text,
     organization text
+);
+
+CREATE TABLE teams (
+    id serial PRIMARY KEY,
+    name text UNIQUE NOT NULL,
+    rating real NOT NULL DEFAULT 0,
+    contest_cnt integer NOT NULL DEFAULT 0,
+    member_1 serial REFERENCES contestants (id),
+    member_2 serial REFERENCES contestants (id),
+    member_3 serial REFERENCES contestants (id),
+    country text,
+    organization text
+);
+
+CREATE TABLE teams_aliases (
+    id serial PRIMARY KEY,
+    name text UNIQUE NOT NULL,
+    team_id serial REFERENCES teams (id)
 );
 
 CREATE TABLE contests (
@@ -38,8 +38,8 @@ CREATE TABLE contests (
 
 CREATE TABLE contests_results (
     id serial PRIMARY KEY,
-    contest_id serial NOT NULL,
-    team_id serial,
+    contest_id serial NOT NULL REFERENCES contests (id),
+    team_id serial REFERENCES teams (id),
     place integer,
     problems_solved integer,
     team_name text NOT NULL,
@@ -48,4 +48,11 @@ CREATE TABLE contests_results (
 );
 
 
+CREATE INDEX contestants_names ON contestants (name);
+
+CREATE INDEX teams_aliases_names ON teams_aliases (name);
+
+CREATE INDEX contests_titles ON contests (title);
+
+CREATE INDEX contests_results_contest_id ON contests_results (contest_id);
 
